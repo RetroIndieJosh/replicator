f7cb3d09eb8862ae4539bae5afe9f4d8146dd4f2
Joshua McLean <mrjoshuamclean@gmail.com>
2020-05-09 21:17:35 -0400

    Transparency in enemy texture

---
+++ Assets/DestroyOnStop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnStop : MonoBehaviour
+{
+    private Rigidbody m_body = null;
+
+    private void Awake() {
+        m_body = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (m_body.velocity.magnitude < Mathf.Epsilon)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/LockRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockRotation : MonoBehaviour
+{
+    private Vector3 m_relativePos;
+    private Quaternion m_rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rotation = transform.rotation;
+        m_relativePos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = m_rotation;
+        transform.localPosition = m_relativePos;
+    }
+}
